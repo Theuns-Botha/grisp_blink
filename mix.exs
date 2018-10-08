409f0ff806d720708af0be86cca6c24b4a80e3b0
Theuns Botha <toensbotes@gmail.com>
2018-10-08 21:01:12 +0200

    First commit

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule GrispBlink.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :grisp_blink,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {GrispBlink.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
